f1b4561f9a291d39392334770cfe675825e49758
xiecanhao <513693233@qq.com>
2018-04-16 18:25:22 +0800

    aaa

--- src/main/java/cn/gz/xchao/first/test/Main.java
+++ src/main/java/cn/gz/xchao/first/test/Main.java
@@ -1,7 +1,7 @@
 package cn.gz.xchao.first.test;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("test meger");
-	}
+    public static void main(String[] args) {
+        System.out.println("test meger");
+    }
 }
